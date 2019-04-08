5dcbb7f5f3bdf2a4323d0b1e48394bf58f700ebe
Thomas Tse <thomasxd24@hotmail.com>
2019-04-08 09:52:21 +0200

    added partially dynamic load product, added dynamic add basketlist

--- GestionCaisseInterBDE/Model/Product.cs
+++ GestionCaisseInterBDE/Model/Product.cs
@@ -14,10 +14,16 @@ namespace GestionCaisseInterBDE.Model
         public int id { get; set; }
         public string name { get; set; }
 
-        public Product(int id,string name)
+        public float price { get; set; }
+
+        public string imageUrl { get; set; }
+
+        public Product(int id,string name,float price ,string imageUrl)
         {
             this.id = id;
             this.name = name;
+            this.price = price;
+            this.imageUrl = imageUrl;
         }
         public static List<Product> getProductList()
         {
@@ -25,9 +31,13 @@ namespace GestionCaisseInterBDE.Model
             var db = new MySQLDatabase("SERVER=51.68.230.58;Database=bde;Uid=bdeUser;Pwd=412qIrJSUkM0;", "MySql.Data.MySqlClient");
             DataTable dt = db.Select("SELECT * FROM products");
             List<Product> products = new List<Product>();
-            foreach(DataRow dr in dt.Rows)
+            foreach (DataRow dr in dt.Rows)
             {
-                Product product = new Product(int.Parse(dr["idProduct"].ToString()), dr["nameProduct"].ToString());
+                Product product = new Product(
+                    int.Parse(dr["idProduct"].ToString()),
+                    dr["nameProduct"].ToString(),
+                    float.Parse(dr["prix"].ToString()),
+                    dr["imageUrl"].ToString());
                 products.Add(product);
             }
             return products;
--- GestionCaisseInterBDE/Views/CaisseScreen.xaml.cs
+++ GestionCaisseInterBDE/Views/CaisseScreen.xaml.cs
@@ -30,6 +30,19 @@ namespace GestionCaisseInterBDE.Views
 
             InitializeComponent();
             List<Product> listProd = Product.getProductList();
+            fillProductPanel(listProd);
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            addProductToBasket(new Product(1, "ds", 5.50f, ""));
+        }
+
+        private void fillProductPanel(List<Product> listProd)
+        {
             productPanel.Children.Clear();
             foreach (Product p in listProd)
             {
@@ -38,15 +51,41 @@ namespace GestionCaisseInterBDE.Views
                 tile.Width = 150;
                 tile.Title = p.name;
                 tile.Tag = p.id;
+                var dp = new DockPanel();
+                dp.Width = 150;
+                dp.Margin = new Thickness(0, 0, 0, 30);
+                var price = new Label();
+                price.Content = p.price.ToString("C2");
+                price.HorizontalAlignment = HorizontalAlignment.Right;
+                DockPanel.SetDock(price, Dock.Top);
+                dp.Children.Add(price);
+                var imageControl = new Image();
+                imageControl.Source = new BitmapImage(new Uri(p.imageUrl));
+                dp.Children.Add(imageControl);
+                tile.Content = dp;
+
                 productPanel.Children.Add(tile);
             }
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void addProductToBasket(Product p)
         {
-            
-
+            foreach (ListViewItem anItem in basketListView.Items)
+            {
+                if (anItem.Content.GetType() != typeof(DockPanel)) return;
+                DockPanel dp = (DockPanel)anItem.Content;
+                foreach(Label lb in dp.Children)
+                {
+                    if (lb.Tag == null) continue;
+                    if (lb.Tag.ToString() == "Quantity")
+                    {
+                        int oldQuantity = int.Parse(lb.Content.ToString().Remove(0, 1)); //TODO EXCPETIONS
+                        string newString = "x" + (oldQuantity + 1).ToString();
+                        lb.Content = newString;
+                    } 
+                }
+            }
         }
     }
 }
