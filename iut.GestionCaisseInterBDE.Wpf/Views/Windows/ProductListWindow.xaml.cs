cb7484c53a72e7b448184f76f03372454800c31e
chtse <thomasxd24@hotmail.com>
2019-05-28 15:05:40 +0200

    fixed some stuff

--- iut.GestionCaisseInterBDE.Wpf/Views/Windows/ProductListWindow.xaml.cs
+++ iut.GestionCaisseInterBDE.Wpf/Views/Windows/ProductListWindow.xaml.cs
@@ -30,9 +30,6 @@ namespace GestionCaisseInterBDE.Windows
         public ProductListWindow()
         {
             InitializeComponent();
-            //var db = new MySQLDatabase("SERVER=51.68.230.58;Port=8080;Database=bde;Uid=bdeUser;Pwd=412qIrJSUkM0;", "MySql.Data.MySqlClient");
-            //DataTable dt = db.Select("SELECT dateCreated AS Date,bde.name AS Encaisseur,quantity AS Qte,round(products.prix*quantity,2) AS Total FROM `ligneTicket`, `bde`,`products` where bde.idBDE = ligneTicket.idBDE and ligneTicket.idProduit=products.idProduct");
-            //testDG.ItemsSource = dt.DefaultView;
             this.DataContext = new ProductListViewModel(DialogCoordinator.Instance);
         }
 
